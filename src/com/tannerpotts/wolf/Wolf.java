50e74f2098a809044d418bfa03dc0783f358c413
Tanner Potts <trifexo@gmail.com>
2016-11-08 19:43:22 -0500

    Added dependencies to project

--- src/com/tannerpotts/wolf/Wolf.java
+++ src/com/tannerpotts/wolf/Wolf.java
@@ -11,11 +11,12 @@ public class Wolf extends JavaPlugin{
     @Override
     public void onDisable() {
         super.onDisable();
-        getLogger().info("Wolf Starting!");
+
     }
 
     @Override
     public void onEnable() {
         super.onEnable();
+        getLogger().info("Wolf Starting!");
     }
 }
